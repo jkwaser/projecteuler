c8a5822513dcae371d3ca3c1ed3053c4099eb5e1
occipitallobe <jkwaser@gmail.com>
2019-11-19 13:48:16 +1000

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,6 @@
+i = [x+1 for x in range(1000)]
+for n in i:
+    if n % 5 == 0 and n % 3 == 0:  # So this is the first programming I've done in a very long time. I'm sure there's a better way to do this.
+        print(n)
+        
+       
